92ae5aece11ebaea7e2ffa33de25c869696e24d2
Shemar Stewart <146385354+Shemar786@users.noreply.github.com>
2024-03-26 09:05:23 -0400

    Add files via upload

---
+++ styles.js
@@ -0,0 +1,7 @@
+// Example: Adding a simple alert when a button is clicked
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('Button clicked!');
+    
+<script src="script.js"></script>
+
+});
